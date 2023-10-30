6216fa42d872590c521f9ae90e83ff5f44ddf0c5
Andy18j <115467259+Andy18j@users.noreply.github.com>
2023-10-30 19:23:34 +0530

    adf

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const {doctorRouter} = require("./routes/doctor.routes")
 const cors = require("cors")
 require("dotenv").config()
 
-app.use(cors())
+
 const app = express()
 app.use(express.json())
-
+app.use(cors())
 
 app.use("/user",userRouter)
 app.use("",doctorRouter)
